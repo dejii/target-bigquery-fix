141b255273f46f826cf64b797cc44e3c196ec6b4
Bo Lu <lv.patrick@gmail.com>
2024-10-30 22:56:43 +1100

    add string to json conversion

--- target_bigquery/schema.py
+++ target_bigquery/schema.py
@@ -1,7 +1,7 @@
 """
 the purpose of this module is to convert JSON schema to BigQuery schema.
 """
-import re
+import re, json
 
 from target_bigquery.simplify_json_schema import BQ_DECIMAL_SCALE_MAX, BQ_BIGDECIMAL_SCALE_MAX, \
     BQ_DECIMAL_MAX_PRECISION_INCREMENT, BQ_BIGDECIMAL_MAX_PRECISION_INCREMENT
@@ -473,7 +473,11 @@ def format_record_to_schema(record, bq_schema):
                 record[k] = [conversion_dict[bq_schema[k]["type"]](vi) for vi in v]
             else:
                 try:
-                    record[k] = conversion_dict[bq_schema[k]["type"]](v)
+                    col_type = bq_schema[k]["type"]
+                    if col_type == "JSON" and isinstance(v, str):
+                        record[k] = json.loads(v)
+                    else:
+                        record[k] = conversion_dict[col_type](v)
                 except:
                     raise Exception(f"===> data conversion error: column={k}, column_type={bq_schema[k]['type']}, value={v}")
     return record
